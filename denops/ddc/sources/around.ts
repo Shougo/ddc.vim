cc1d0105c6bcc1ea024de01f1de07c612565caae
octaltree <octaltree@users.noreply.github.com>
2021-06-28 00:46:57 +0900

    around concurrent

--- denops/ddc/sources/around.ts
+++ denops/ddc/sources/around.ts
@@ -1,27 +1,36 @@
 import { BaseSource } from "../base/source.ts";
 import { Candidate } from "../types.ts";
 import { Denops } from "../deps.ts";
+import { imap, range } from "https://deno.land/x/itertools@v0.1.2/mod.ts";
+
+export function splitPages(
+  maxLines: number,
+  size: number,
+): Iterable<[number, number]> {
+  return imap(
+    range(1, /* < */ maxLines + 1, size),
+    (lnum: number) => [lnum, /* <= */ lnum + size - 1],
+  );
+}
+
+export function allWords(lines: string[]): string[] {
+  return lines.flatMap((line) => [...line.matchAll(/[a-zA-Z0-9_]+/g)])
+    .map((match) => match[0]);
+}
 
 export class Source extends BaseSource {
   async gatherCandidates(denops: Denops): Promise<Candidate[]> {
-    const candidates: Candidate[] = [];
-    let lines: string[] = [];
-
-    const count = 500;
+    const pageSize = 500;
     const maxLines = (await denops.call("line", "$")) as number;
-    for (let i = 1; i <= maxLines; i += count) {
-      lines = await denops.call(
-        "getline",
-        i,
-        i + count,
-      ) as string[];
-      lines.forEach((line) => {
-        [...line.matchAll(/[a-zA-Z0-9_]+/g)].forEach((match) => {
-          candidates.push({ word: match[0] });
-        });
-      });
-    }
+    const pages = (await Promise.all(
+      imap(
+        splitPages(maxLines, pageSize),
+        ([start, end]: [number, number]) => denops.call("getline", start, end),
+      ),
+    )) as string[][];
+    const lines = pages.flatMap((page) => page);
 
+    const candidates: Candidate[] = allWords(lines).map((word) => ({ word }));
     return candidates;
   }
 }
