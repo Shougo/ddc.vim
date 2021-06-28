b27bd2a18a27f54309829669af05e2ed197abe63
octaltree <octaltree@users.noreply.github.com>
2021-06-28 11:50:47 +0900

    fix errors

--- denops/ddc/filters/matcher_head.ts
+++ denops/ddc/filters/matcher_head.ts
@@ -1,13 +1,13 @@
 import { BaseFilter } from "../base/filter.ts";
 import { Candidate, Context } from "../types.ts";
-import { Vim } from "../deps.ts";
+import { Denops } from "../deps.ts";
 
-export class Filter implements BaseFilter {
-  async filter(_vim: Vim, context: Context): Candidate[] {
+export class Filter extends BaseFilter {
+  filter(_denops: Denops, context: Context): Promise<Candidate[]> {
     const completeStr = context.input.match(/\w*$/);
     const candidates = context.candidates.filter(
       (candidate) => candidate.word.indexOf(completeStr) == 0,
     );
-    return await candidates;
+    return Promise.resolve(candidates);
   }
 }
