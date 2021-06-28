f3ae7ffb1854b6f67b63bde127d182cece7e43a0
octaltree <octaltree@users.noreply.github.com>
2021-06-28 12:19:38 +0900

    test matcher_head

--- denops/ddc/filters/matcher_head.ts
+++ denops/ddc/filters/matcher_head.ts
@@ -1,13 +1,27 @@
 import { BaseFilter } from "../base/filter.ts";
 import { Candidate, Context } from "../types.ts";
 import { Denops } from "../deps.ts";
+import { assertEquals } from "https://deno.land/std@0.98.0/testing/asserts.ts";
+
+function lastWord(input: string): string {
+  const match = input.match(/\w*$/);
+  return match ? match[0] : "";
+}
 
 export class Filter extends BaseFilter {
   filter(_denops: Denops, context: Context): Promise<Candidate[]> {
-    const completeStr = context.input.match(/\w*$/);
+    const completeStr = lastWord(context.input);
     const candidates = context.candidates.filter(
-      (candidate) => candidate.word.indexOf(completeStr) == 0,
+      (candidate) => candidate.word.startsWith(completeStr),
     );
     return Promise.resolve(candidates);
   }
 }
+
+Deno.test("lastWord", () => {
+  assertEquals(lastWord(""), "");
+  assertEquals(lastWord("a"), "a");
+  assertEquals(lastWord("as df"), "df");
+  assertEquals(lastWord("as dfあ"), "");
+  assertEquals(lastWord("as dfあwer"), "wer");
+});
