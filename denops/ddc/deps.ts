d9836a91f9d841244550528254c7d4350a6840f9
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-07-12 08:37:51 +0900

    Version up deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,5 +1,5 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.5/mod.ts#^";
-export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.5/helper/mod.ts#^";
-export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.5/variable/mod.ts#^";
-export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.5/autocmd/mod.ts#^";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.7/mod.ts#^";
+export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.7/helper/mod.ts#^";
+export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.7/variable/mod.ts#^";
+export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.7/autocmd/mod.ts#^";
 export { ensureObject } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts#^";
