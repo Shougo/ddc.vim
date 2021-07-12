804891938cd60d8afbc767cc5df4e83987d985c8
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-07-12 23:27:55 +0900

    Add udd marker

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -3,4 +3,4 @@ export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.7/helper/mod
 export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.7/variable/mod.ts#^";
 export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.7/autocmd/mod.ts#^";
 export { ensureObject } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts#^";
-export { assertEquals } from "https://deno.land/std@0.100.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.100.0/testing/asserts.ts#^";
