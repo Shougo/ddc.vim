1b17721b720738c50ec2926702aecff3c9bcdfb2
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-07-13 13:23:57 +0900

    Version up deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.7/mod.ts#^";
-export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.7/helper/mod.ts#^";
-export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.7/variable/mod.ts#^";
-export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.7/autocmd/mod.ts#^";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.8/mod.ts#^";
+export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.8/helper/mod.ts#^";
+export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.8/variable/mod.ts#^";
+export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.8/autocmd/mod.ts#^";
 export { ensureObject } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts#^";
-export { assertEquals } from "https://deno.land/std@0.100.0/testing/asserts.ts#^";
+export { assertEquals } from "https://deno.land/std@0.101.0/testing/asserts.ts#^";
