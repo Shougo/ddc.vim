f8286e0138ae5f11656c86619240cd237e200915
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-08-08 15:35:07 +0900

    version up

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,11 +1,11 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts#^";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.1/mod.ts#^";
 export {
   batch,
   execute,
-} from "https://deno.land/x/denops_std@v1.0.0/helper/mod.ts#^";
-export * as fn from "https://deno.land/x/denops_std@v1.0.0/function/mod.ts#^";
-export * as vars from "https://deno.land/x/denops_std@v1.0.0/variable/mod.ts#^";
-export * as autocmd from "https://deno.land/x/denops_std@v1.0.0/autocmd/mod.ts#^";
+} from "https://deno.land/x/denops_std@v1.0.1/helper/mod.ts#^";
+export * as fn from "https://deno.land/x/denops_std@v1.0.1/function/mod.ts#^";
+export * as vars from "https://deno.land/x/denops_std@v1.0.1/variable/mod.ts#^";
+export * as autocmd from "https://deno.land/x/denops_std@v1.0.1/autocmd/mod.ts#^";
 export { ensureObject } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts#^";
 export { assertEquals } from "https://deno.land/std@0.103.0/testing/asserts.ts#^";
 export { toFileUrl } from "https://deno.land/std@0.103.0/path/mod.ts#^";
