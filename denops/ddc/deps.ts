1fe78a3de4762ac07a8eb34eaf05eb6b184607a4
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-09-24 19:30:45 +0900

    Version up deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -9,10 +9,10 @@ export * as fn from "https://deno.land/x/denops_std@v1.11.3/function/mod.ts#^";
 export * as vars from "https://deno.land/x/denops_std@v1.11.3/variable/mod.ts#^";
 export * as autocmd from "https://deno.land/x/denops_std@v1.11.3/autocmd/mod.ts#^";
 export { ensureObject } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts#^";
-export { assertEquals } from "https://deno.land/std@0.107.0/testing/asserts.ts#^";
-export { parse, toFileUrl } from "https://deno.land/std@0.107.0/path/mod.ts#^";
+export { assertEquals } from "https://deno.land/std@0.108.0/testing/asserts.ts#^";
+export { parse, toFileUrl } from "https://deno.land/std@0.108.0/path/mod.ts#^";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.107.0/async/mod.ts#^";
-export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.0/response_waiter.ts#^";
+} from "https://deno.land/std@0.108.0/async/mod.ts#^";
+export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.1/response_waiter.ts#^";
