6accccbc1a3bc1d51711302141e981d181601876
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-10-09 00:30:11 +0900

    Version up

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,18 +1,18 @@
-export type { Denops } from "https://deno.land/x/denops_std@v2.0.0/mod.ts#^";
-export { execute } from "https://deno.land/x/denops_std@v2.0.0/helper/mod.ts#^";
+export type { Denops } from "https://deno.land/x/denops_std@v2.1.0/mod.ts#^";
+export { execute } from "https://deno.land/x/denops_std@v2.1.0/helper/mod.ts#^";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v2.0.0/batch/mod.ts#^";
-export * as op from "https://deno.land/x/denops_std@v2.0.0/option/mod.ts#^";
-export * as fn from "https://deno.land/x/denops_std@v2.0.0/function/mod.ts#^";
-export * as vars from "https://deno.land/x/denops_std@v2.0.0/variable/mod.ts#^";
-export * as autocmd from "https://deno.land/x/denops_std@v2.0.0/autocmd/mod.ts#^";
+} from "https://deno.land/x/denops_std@v2.1.0/batch/mod.ts#^";
+export * as op from "https://deno.land/x/denops_std@v2.1.0/option/mod.ts#^";
+export * as fn from "https://deno.land/x/denops_std@v2.1.0/function/mod.ts#^";
+export * as vars from "https://deno.land/x/denops_std@v2.1.0/variable/mod.ts#^";
+export * as autocmd from "https://deno.land/x/denops_std@v2.1.0/autocmd/mod.ts#^";
 export { ensureObject } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts#^";
-export { assertEquals } from "https://deno.land/std@0.108.0/testing/asserts.ts#^";
-export { parse, toFileUrl } from "https://deno.land/std@0.108.0/path/mod.ts#^";
+export { assertEquals } from "https://deno.land/std@0.110.0/testing/asserts.ts#^";
+export { parse, toFileUrl } from "https://deno.land/std@0.110.0/path/mod.ts#^";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.108.0/async/mod.ts#^";
-export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.1/response_waiter.ts#^";
+} from "https://deno.land/std@0.110.0/async/mod.ts#^";
+export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.3/response_waiter.ts#^";
