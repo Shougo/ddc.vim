bae6c72715da3081112ae8eee29b42fd0e5d5a4e
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-10-31 11:29:05 +0900

    Update deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,23 +1,23 @@
-export type { Denops } from "https://deno.land/x/denops_std@v2.1.1/mod.ts#^";
-export { execute } from "https://deno.land/x/denops_std@v2.1.1/helper/mod.ts#^";
+export type { Denops } from "https://deno.land/x/denops_std@v2.1.3/mod.ts#^";
+export { execute } from "https://deno.land/x/denops_std@v2.1.3/helper/mod.ts#^";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v2.1.1/batch/mod.ts#^";
-export * as op from "https://deno.land/x/denops_std@v2.1.1/option/mod.ts#^";
-export * as fn from "https://deno.land/x/denops_std@v2.1.1/function/mod.ts#^";
-export * as vars from "https://deno.land/x/denops_std@v2.1.1/variable/mod.ts#^";
-export * as autocmd from "https://deno.land/x/denops_std@v2.1.1/autocmd/mod.ts#^";
+} from "https://deno.land/x/denops_std@v2.1.3/batch/mod.ts#^";
+export * as op from "https://deno.land/x/denops_std@v2.1.3/option/mod.ts#^";
+export * as fn from "https://deno.land/x/denops_std@v2.1.3/function/mod.ts#^";
+export * as vars from "https://deno.land/x/denops_std@v2.1.3/variable/mod.ts#^";
+export * as autocmd from "https://deno.land/x/denops_std@v2.1.3/autocmd/mod.ts#^";
 export {
   ensureObject,
   ensureString,
 } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts#^";
-export * as base64 from "https://deno.land/std@0.111.0/encoding/base64.ts";
-export { assertEquals } from "https://deno.land/std@0.111.0/testing/asserts.ts#^";
-export { parse, toFileUrl } from "https://deno.land/std@0.111.0/path/mod.ts#^";
+export * as base64 from "https://deno.land/std@0.113.0/encoding/base64.ts";
+export { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts#^";
+export { parse, toFileUrl } from "https://deno.land/std@0.113.0/path/mod.ts#^";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.111.0/async/mod.ts#^";
+} from "https://deno.land/std@0.113.0/async/mod.ts#^";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.4/response_waiter.ts#^";
 export { spy } from "https://deno.land/x/mock@0.10.1/spy.ts#^";
