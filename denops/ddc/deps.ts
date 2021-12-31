3c8d3814fd35b657681de5fd72373935847b5e2d
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-12-31 10:40:52 +0900

    Update deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,13 +1,13 @@
-export type { Denops } from "https://deno.land/x/denops_std@v2.2.0/mod.ts#^";
-export { execute } from "https://deno.land/x/denops_std@v2.2.0/helper/mod.ts#^";
+export type { Denops } from "https://deno.land/x/denops_std@v2.4.0/mod.ts#^";
+export { execute } from "https://deno.land/x/denops_std@v2.4.0/helper/mod.ts#^";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v2.2.0/batch/mod.ts#^";
-export * as op from "https://deno.land/x/denops_std@v2.2.0/option/mod.ts#^";
-export * as fn from "https://deno.land/x/denops_std@v2.2.0/function/mod.ts#^";
-export * as vars from "https://deno.land/x/denops_std@v2.2.0/variable/mod.ts#^";
-export * as autocmd from "https://deno.land/x/denops_std@v2.2.0/autocmd/mod.ts#^";
+} from "https://deno.land/x/denops_std@v2.4.0/batch/mod.ts#^";
+export * as op from "https://deno.land/x/denops_std@v2.4.0/option/mod.ts#^";
+export * as fn from "https://deno.land/x/denops_std@v2.4.0/function/mod.ts#^";
+export * as vars from "https://deno.land/x/denops_std@v2.4.0/variable/mod.ts#^";
+export * as autocmd from "https://deno.land/x/denops_std@v2.4.0/autocmd/mod.ts#^";
 export {
   ensureObject,
   ensureString,
