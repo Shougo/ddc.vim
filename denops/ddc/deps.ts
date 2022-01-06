f5ebf50f82f83250e5f84503b6630a52349e2333
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-01-06 13:42:46 +0900

    Update deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -12,12 +12,12 @@ export {
   ensureObject,
   ensureString,
 } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts#^";
-export * as base64 from "https://deno.land/std@0.119.0/encoding/base64.ts";
-export { assertEquals } from "https://deno.land/std@0.119.0/testing/asserts.ts#^";
-export { parse, toFileUrl } from "https://deno.land/std@0.119.0/path/mod.ts#^";
+export * as base64 from "https://deno.land/std@0.120.0/encoding/base64.ts";
+export { assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts#^";
+export { parse, toFileUrl } from "https://deno.land/std@0.120.0/path/mod.ts#^";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.119.0/async/mod.ts#^";
+} from "https://deno.land/std@0.120.0/async/mod.ts#^";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.4/response_waiter.ts#^";
 export { spy } from "https://deno.land/x/mock@0.12.1/spy.ts#^";
