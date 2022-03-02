f89707ed0aaba8468e8cb6e11dcd92ec70622d6e
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-03-02 14:27:46 +0900

    Update deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,13 +1,13 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.1.3/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v3.1.3/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.1.4/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v3.1.4/helper/mod.ts";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v3.1.3/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v3.1.3/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.1.3/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.1.3/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.1.3/autocmd/mod.ts";
+} from "https://deno.land/x/denops_std@v3.1.4/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v3.1.4/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.1.4/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.1.4/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.1.4/autocmd/mod.ts";
 export * as base64 from "https://deno.land/std@0.127.0/encoding/base64.ts";
 export { assertEquals } from "https://deno.land/std@0.127.0/testing/asserts.ts";
 export { parse, toFileUrl } from "https://deno.land/std@0.127.0/path/mod.ts";
