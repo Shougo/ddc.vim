8f6f6c79b829fecb04fe955c41df5213eeb7020a
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-05-21 16:04:14 +0900

    Update deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -8,14 +8,14 @@ export * as op from "https://deno.land/x/denops_std@v3.3.1/option/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v3.3.1/function/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v3.3.1/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v3.3.1/autocmd/mod.ts";
-export * as base64 from "https://deno.land/std@0.137.0/encoding/base64.ts";
-export { assertEquals } from "https://deno.land/std@0.137.0/testing/asserts.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.137.0/path/mod.ts";
+export * as base64 from "https://deno.land/std@0.140.0/encoding/base64.ts";
+export { assertEquals } from "https://deno.land/std@0.140.0/testing/asserts.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.140.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.137.0/async/mod.ts";
-export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.5/response_waiter.ts";
+} from "https://deno.land/std@0.140.0/async/mod.ts";
+export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.6/response_waiter.ts";
 export { spy } from "https://deno.land/x/mock@0.15.2/mock.ts";
 export {
   ensureNumber,
