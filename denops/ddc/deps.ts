1c2fe72cd7b8650659b4cf8a39f0af20cb367573
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-08-05 12:11:33 +0900

    Update deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,23 +1,23 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.6.0/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.7.1/mod.ts";
 export {
   echo,
   execute,
-} from "https://deno.land/x/denops_std@v3.6.0/helper/mod.ts";
+} from "https://deno.land/x/denops_std@v3.7.1/helper/mod.ts";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v3.6.0/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v3.6.0/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.6.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.6.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.6.0/autocmd/mod.ts";
-export * as base64 from "https://deno.land/std@0.150.0/encoding/base64.ts";
-export { assertEquals } from "https://deno.land/std@0.150.0/testing/asserts.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.150.0/path/mod.ts";
+} from "https://deno.land/x/denops_std@v3.7.1/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v3.7.1/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.7.1/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.7.1/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.7.1/autocmd/mod.ts";
+export * as base64 from "https://deno.land/std@0.151.0/encoding/base64.ts";
+export { assertEquals } from "https://deno.land/std@0.151.0/testing/asserts.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.151.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.150.0/async/mod.ts";
+} from "https://deno.land/std@0.151.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.6/response_waiter.ts";
 export { spy } from "https://deno.land/x/mock@0.15.2/mock.ts";
 export {
