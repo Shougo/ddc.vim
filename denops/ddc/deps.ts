d89ed4200d418164c23fbf2c84ae83bd8c81af86
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-05-29 07:40:50 +0900

    Update deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -11,16 +11,16 @@ export * as op from "https://deno.land/x/denops_std@v5.0.0/option/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v5.0.0/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v5.0.0/autocmd/mod.ts";
-export * as base64 from "https://deno.land/std@0.188.0/encoding/base64.ts";
+export * as base64 from "https://deno.land/std@0.177.1/encoding/base64.ts";
 export {
   assertEquals,
   equal,
-} from "https://deno.land/std@0.188.0/testing/asserts.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.188.0/path/mod.ts";
+} from "https://deno.land/std@0.177.1/testing/asserts.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.177.1/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.188.0/async/mod.ts";
+} from "https://deno.land/std@0.177.1/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { spy } from "https://deno.land/x/mock@0.15.2/mock.ts";
 export {
