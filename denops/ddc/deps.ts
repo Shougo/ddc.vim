9572f51b8971973494607e5e8cd96fdff21376d8
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-12-14 09:56:31 +0900

    Update deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,30 +1,30 @@
-export type { Denops } from "https://deno.land/x/denops_std@v5.1.0/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
 export {
   echo,
   execute,
-} from "https://deno.land/x/denops_std@v5.1.0/helper/mod.ts";
+} from "https://deno.land/x/denops_std@v5.2.0/helper/mod.ts";
 export {
   batch,
   collect,
-} from "https://deno.land/x/denops_std@v5.1.0/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v5.1.0/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.1.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v5.1.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v5.1.0/autocmd/mod.ts";
-export * as base64 from "https://deno.land/std@0.208.0/encoding/base64.ts";
+} from "https://deno.land/x/denops_std@v5.2.0/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v5.2.0/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.2.0/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v5.2.0/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v5.2.0/autocmd/mod.ts";
+export * as base64 from "https://deno.land/std@0.209.0/encoding/base64.ts";
 export {
   assertEquals,
   equal,
-} from "https://deno.land/std@0.208.0/assert/mod.ts";
+} from "https://deno.land/std@0.209.0/assert/mod.ts";
 export {
   basename,
   parse,
   toFileUrl,
-} from "https://deno.land/std@0.208.0/path/mod.ts";
+} from "https://deno.land/std@0.209.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.208.0/async/mod.ts";
+} from "https://deno.land/std@0.209.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { spy } from "https://deno.land/x/mock@0.15.2/mock.ts";
 export { ensure, is } from "https://deno.land/x/unknownutil@v3.11.0/mod.ts";
