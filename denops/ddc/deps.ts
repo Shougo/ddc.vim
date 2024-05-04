a383f34664280bffe4ecd197ee630241527c6a59
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-05-04 11:38:52 +0900

    Use JSR imports

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -12,20 +12,10 @@ export * as vimOp from "https://deno.land/x/denops_std@v6.4.0/option/vim/mod.ts"
 export * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts";
-export * as base64 from "https://deno.land/std@0.224.0/encoding/base64.ts";
-export {
-  assertEquals,
-  equal,
-} from "https://deno.land/std@0.224.0/assert/mod.ts";
-export {
-  basename,
-  parse,
-  toFileUrl,
-} from "https://deno.land/std@0.224.0/path/mod.ts";
-export {
-  deadline,
-  DeadlineError,
-} from "https://deno.land/std@0.224.0/async/mod.ts";
+export * from "jsr:@std/encoding@0.224.0/base64";
+export { assertEquals, equal } from "jsr:@std/assert@0.224.0";
+export { basename, parse, toFileUrl } from "jsr:@std/path@0.224.0";
+export { deadline, DeadlineError } from "jsr:@std/async@0.224.0";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { spy } from "https://deno.land/x/mock@0.15.2/mock.ts";
 export { ensure, is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
