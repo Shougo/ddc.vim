59386ec39d62986d8de7f261844ca48ad22fccf7
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-07-27 13:43:37 +0900

    Fix format

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,15 +1,6 @@
-export type {
-  Denops,
-  Entrypoint,
-} from "jsr:@denops/std@7.0.0-pre2";
-export {
-  echo,
-  execute,
-} from "jsr:@denops/std@7.0.0-pre2/helper";
-export {
-  batch,
-  collect,
-} from "jsr:@denops/std@7.0.0-pre2/batch";
+export type { Denops, Entrypoint } from "jsr:@denops/std@7.0.0-pre2";
+export { echo, execute } from "jsr:@denops/std@7.0.0-pre2/helper";
+export { batch, collect } from "jsr:@denops/std@7.0.0-pre2/batch";
 export * as op from "jsr:@denops/std@7.0.0-pre2/option";
 export * as vimOp from "jsr:@denops/std@7.0.0-pre2/option/vim";
 export * as fn from "jsr:@denops/std@7.0.0-pre2/function";
