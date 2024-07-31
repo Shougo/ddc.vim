ea74600178a86449bc35804b75ba6a0a02cdf8ff
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-07-31 13:07:58 +0900

    Update denops/std

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,11 +1,11 @@
-export type { Denops, Entrypoint } from "jsr:@denops/std@7.0.0";
-export { echo, execute } from "jsr:@denops/std@7.0.0/helper";
-export { batch, collect } from "jsr:@denops/std@7.0.0/batch";
-export * as op from "jsr:@denops/std@7.0.0/option";
-export * as vimOp from "jsr:@denops/std@7.0.0/option/vim";
-export * as fn from "jsr:@denops/std@7.0.0/function";
-export * as vars from "jsr:@denops/std@7.0.0/variable";
-export * as autocmd from "jsr:@denops/std@7.0.0/autocmd";
+export type { Denops, Entrypoint } from "jsr:@denops/std@7.0.1";
+export { echo, execute } from "jsr:@denops/std@7.0.1/helper";
+export { batch, collect } from "jsr:@denops/std@7.0.1/batch";
+export * as op from "jsr:@denops/std@7.0.1/option";
+export * as vimOp from "jsr:@denops/std@7.0.1/option/vim";
+export * as fn from "jsr:@denops/std@7.0.1/function";
+export * as vars from "jsr:@denops/std@7.0.1/variable";
+export * as autocmd from "jsr:@denops/std@7.0.1/autocmd";
 
 export * from "jsr:@std/encoding@1.0.1/base64";
 export { assertEquals, equal } from "jsr:@std/assert@1.0.1";
