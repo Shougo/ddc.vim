20b05f63cdeb1a956ff10164308f533a070e725b
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-08-27 09:39:35 +0900

    Update deps

--- denops/ddc/deps.ts
+++ denops/ddc/deps.ts
@@ -1,11 +1,11 @@
-export type { Denops, Entrypoint } from "jsr:@denops/std@~7.0.1";
-export { echo, execute } from "jsr:@denops/std@~7.0.1/helper";
-export { batch, collect } from "jsr:@denops/std@~7.0.1/batch";
-export * as op from "jsr:@denops/std@~7.0.1/option";
-export * as vimOp from "jsr:@denops/std@~7.0.1/option/vim";
-export * as fn from "jsr:@denops/std@~7.0.1/function";
-export * as vars from "jsr:@denops/std@~7.0.1/variable";
-export * as autocmd from "jsr:@denops/std@~7.0.1/autocmd";
+export type { Denops, Entrypoint } from "jsr:@denops/std@~7.1.0";
+export { echo, execute } from "jsr:@denops/std@~7.1.0/helper";
+export { batch, collect } from "jsr:@denops/std@~7.1.0/batch";
+export * as op from "jsr:@denops/std@~7.1.0/option";
+export * as vimOp from "jsr:@denops/std@~7.1.0/option/vim";
+export * as fn from "jsr:@denops/std@~7.1.0/function";
+export * as vars from "jsr:@denops/std@~7.1.0/variable";
+export * as autocmd from "jsr:@denops/std@~7.1.0/autocmd";
 
 export * from "jsr:@std/encoding@~1.0.1/base64";
 export { assertEquals, equal } from "jsr:@std/assert@~1.0.1";
@@ -13,5 +13,5 @@ export { basename, parse, toFileUrl } from "jsr:@std/path@~1.0.2";
 export { deadline } from "jsr:@std/async@~1.0.1";
 export { spy } from "jsr:@std/testing@~1.0.0/mock";
 
-export { ensure, is } from "jsr:@core/unknownutil@~4.0.0";
+export { ensure, is } from "jsr:@core/unknownutil@~4.3.0";
 export { Lock } from "jsr:@lambdalisue/async@~2.1.1";
