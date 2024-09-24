657e2365d2865e5774c095556098fb2a9aeace4a
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-09-24 09:39:11 +0900

    Fix type error

--- denops/@ddu-sources/ddc.ts
+++ denops/@ddu-sources/ddc.ts
@@ -1,7 +1,8 @@
-import type { Context, Denops, Item } from "jsr:@shougo/ddu-vim@6.2.0/types";
+import type { Context, Item } from "jsr:@shougo/ddu-vim@6.2.0/types";
 import { BaseSource } from "jsr:@shougo/ddu-vim@6.2.0/source";
 import type { DdcItem } from "../ddc/types.ts";
 
+import type { Denops } from "jsr:@denops/std@~7.1.0";
 import * as vars from "jsr:@denops/std@~7.1.0/variable";
 
 type Params = Record<string, never>;
