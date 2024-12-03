0743f16a9e8a34b5c421cf9dcbaef76281af73a4
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-12-03 20:08:32 +0900

    Update ddu.vim version

--- denops/@ddu-sources/ddc.ts
+++ denops/@ddu-sources/ddc.ts
@@ -1,5 +1,5 @@
-import type { Context, Item } from "jsr:@shougo/ddu-vim@8.0.0/types";
-import { BaseSource } from "jsr:@shougo/ddu-vim@8.0.0/source";
+import type { Context, Item } from "jsr:@shougo/ddu-vim@9.0.0/types";
+import { BaseSource } from "jsr:@shougo/ddu-vim@9.0.0/source";
 import type { DdcItem } from "../ddc/types.ts";
 
 import type { Denops } from "jsr:@denops/std@~7.4.0";
