492884a7f7a472a319bc08e82e8ae5def264462d
Shougo Matsushita <Shougo.Matsu@gmail.com>
2026-03-25 08:50:40 +0900

    Fix

--- denops/ddc/utils.ts
+++ denops/ddc/utils.ts
@@ -136,7 +136,8 @@ export async function printError(
 ) {
   const message = messages.map((v) => {
     if (v instanceof Error) {
-      // NOTE: In Deno, Prefer `Error.stack` because it contains `Error.message`.
+      // NOTE: In Deno, Prefer `Error.stack` because it contains
+      // `Error.message`.
       return `${v.stack ?? v}`;
     } else if (typeof v === "object") {
       return JSON.stringify(v);
